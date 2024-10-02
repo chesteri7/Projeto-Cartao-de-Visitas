129f16b88da98c190a59a7d2bb8aca45a93cc1f7
Chestao <chesterlopes65@gmail.com>
2024-10-02 19:55:53 -0300

    Algoritimo

---
+++ Todos os Projetos/Exercicios Logica e Algoritimo com JavaScript/exercicios.js
@@ -0,0 +1,51 @@
+//soma de dois numeros//
+var numero1 = 5
+var numero2 = 10
+var soma = numero1 + numero2
+
+
+
+//subtração de dois numeros//
+var numero1 = 15
+var numero2 = 7
+var subtracao = numero1 - numero2
+
+//multiplicação de dois numeros//
+var numero1 = 5
+var numero2 = 7
+var multiplicacao = numero1 * numero2
+
+//divisão de dois numeros//
+var numero1 = 24
+var numero2 = 2
+var divisao = numero1 / numero2
+
+//resto da divisão de dois numeros//
+var numero1 = 10
+var numero2 = 3
+var resto = numero1 % numero2
+
+
+//como verificar se a soma de dois numeros é par ou impar//
+var numero1 = 18
+var numero2 = 5
+var soma = numero1 + numero2
+if(soma % 2 === 0) {
+    console.log("A soma é par.")
+} else {
+    console.log("Á soma é impar ")
+}
+
+
+//como verificar se os dois numeros inseridos são iguais ou diferentes//
+var numero1 = 20
+var numero2 = 20
+if(numero1 === numero2) {
+    console.log("Os numeros são iguais.")
+} else {
+    console.log("Os numeros são diferentes")
+}
+
+
+
+
--- Todos os Projetos/Projeto Adiciona e Remove Tarefa/main.js
+++ Todos os Projetos/Projeto Adiciona e Remove Tarefa/main.js
@@ -1,19 +1,25 @@
 function addNewTarefa(){
+  // Obter o elemento ul
   var List = document.getElementById("List");
+  // Obter o texto do input
   var text = document.getElementById("nome da tarefa").value;
+  // isso verifica se o texto escrito no input tem mais de 1 caracter
   if(text.length === 0) {
     alert("Essa Tarefa Precisa Conter Mais de 1 Caracter");
     return;
   }
+  // isso cria um lista de itens
   var ListItem = document.createElement("li");
   ListItem.className = "list-item"
-  
+  // iiso cria um texto na pagina e adiciona  na lista
   const textElement = document.createTextNode(text);
   ListItem.appendChild(textElement)
   List.appendChild(ListItem)
+  document.getElementById("nome da tarefa").value ="";
 }
 
 function deleteTarefa(){
+  // isso apaga todo texto criado na lista
     var List = document.getElementById("List");
     List.innerHTML = "";
 }
